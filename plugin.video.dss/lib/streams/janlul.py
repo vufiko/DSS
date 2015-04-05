11d0f4a12a0dda8c077232e308d496866f966f6f
dutchsportstreams <patrick@paradiseradio.org>
2015-04-05 11:44:37 +0200

    update

--- plugin.video.dss/lib/streams/janlul.py
+++ plugin.video.dss/lib/streams/janlul.py
@@ -28,14 +28,9 @@ def addStreams():
     #xbmcutil.addMenuItem('[COLOR '+color+']JanLul.com - Stream 2[/COLOR]', janlul2, 'true', 'janlul', 'janlul')
 
     xbmcutil.updateProgressBar(pBar, 48, 'JanLul 3')
-    #jl_stream3 = bitly.getLink('janlul3', sourceSite)
-    #veetle.addChannel('JanLul.com - Stream 3', jl_stream3, 'janlul')
-    janlul3 = 'http://stream.ssh101.com/hls/janlul6.m3u8'
-    if(xbmcutil.getResponse(janlul3)):
-        color = 'green'
-    else :
-        color = 'red'
-    xbmcutil.addMenuItem('[COLOR '+color+']JanLul.com - Stream 3[/COLOR]', janlul3, 'true', 'janlul', 'janlul')
+    jl_stream3 = bitly.getLink('janlul3', sourceSite)
+    veetle.addChannel('JanLul.com - Stream 3', jl_stream3, 'janlul')
+    
 
     xbmcutil.updateProgressBar(pBar, 64, 'JanLul 4')
     jl_stream4 = bitly.getLink('janlul4', sourceSite)
@@ -46,14 +41,14 @@ def addStreams():
     veetle.addChannel('JanLul.com - Stream 5', jl_stream5, 'janlul')
 
     xbmcutil.updateProgressBar(pBar, 96, 'JanLul 6')
-    jl_stream6 = bitly.getLink('janlul6', sourceSite)
-    veetle.addChannel('JanLul.com - Stream 6', jl_stream6, 'janlul')
-    #janlul6 = 'http://stream.ssh101.com/hls/janlul6.m3u8'
-    #if(xbmcutil.getResponse(janlul6)):
-    #    color = 'green'
-    #else :
-    #    color = 'red'
-    #xbmcutil.addMenuItem('[COLOR '+color+']JanLul.com - Stream 6[/COLOR]', janlul6, 'true', 'janlul', 'janlul')
+    #jl_stream6 = bitly.getLink('janlul6', sourceSite)
+    #veetle.addChannel('JanLul.com - Stream 6', jl_stream6, 'janlul')
+    janlul6 = 'http://stream.ssh101.com/hls/janlul6.m3u8'
+    if(xbmcutil.getResponse(janlul6)):
+        color = 'green'
+    else :
+        color = 'red'
+    xbmcutil.addMenuItem('[COLOR '+color+']JanLul.com - Stream 6[/COLOR]', janlul6, 'true', 'janlul', 'janlul')
     
     
     xbmcutil.updateProgressBar(pBar, 100,'Gereed!')
