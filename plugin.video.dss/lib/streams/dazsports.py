a292c60692273b14f226f7d57354056cce931ac5
dutchsportstreams <patrick@paradiseradio.org>
2015-05-09 18:09:15 +0200

    update

--- plugin.video.dss/lib/streams/dazsports.py
+++ plugin.video.dss/lib/streams/dazsports.py
@@ -1,63 +1,51 @@
 from ..utils import bitly, xbmcutil
-from . import veetle
-import re
+from . import veetle, sopcast
+import urllib2, re
 
 sourceSite='http://dazsports.org'
 
 def addStreams():
     pBar = xbmcutil.createProgressBar('Dutch Sport Streams', 'Laden van streams...')
-
+    xbmcutil.updateProgressBar(pBar, 33, 'DazSports 3')
+    addStream('player3','DazSports - Stream 3')
+   
+    xbmcutil.updateProgressBar(pBar, 66, 'DazSports 4')
+    addStream('player4','DazSports - Stream 4')
     
-    xbmcutil.updateProgressBar(pBar, 30, 'DazSports 3')
-    addStream('streamplayer3', 'DazSports - Stream 3')
-
-    
-    xbmcutil.updateProgressBar(pBar, 49, 'DazSports 4')
-    addStream('streamplayer4', 'DazSports - Stream 4')
-
-    xbmcutil.updateProgressBar(pBar, 98, 'DazSports 5')
-    addStream('streamplayer5', 'DazSports - Stream 5')
-
-    
-    xbmcutil.updateProgressBar(pBar, 100,'Gereed!')
+    xbmcutil.updateProgressBar(pBar, 99, 'DazSports 5')
+    addStream('player5','DazSports - Stream 5')
     xbmcutil.endOfList()
 
-
-
+    
 def addStream(stream, display) :
     streamUrl = findStream(stream) 
     if streamUrl[-4:] == '.flv' :
-        print('Veetle')
-        veetle.addChannel(display, streamUrl, 'DazSports')
+        veetle.addChannel(display, streamUrl, 'daz')
     else :
-        print('M3U')
-        if (xbmcutil.getResponse(streamUrl)) :
+        if bitly.getResponse(streamUrl) :
             color = 'green'
         else :
             streamUrl = ''
             color = 'red'
-        xbmcutil.addMenuItem('[COLOR '+color+']'+display+'[/COLOR]', streamUrl, 'true', 'DazSports','DazSports')
+        xbmcutil.addMenuItem('[COLOR '+color+']'+display+'[/COLOR]', streamUrl, 'true', 'daz','daz')
 
 
 def findStream(page) :
-    page1 = (sourceSite + '/' + page +'.php')
-    if(page1[:4] != 'http') :
-        page1 = sourceSite + '/' + page1
-    frameHtml = bitly.getPage(page1, sourceSite, bitly.getUserAgent())
-    b64coded = bitly.getBaseEncodedString(frameHtml)
-    print b64coded
+    ua = bitly.getUserAgent()
+    page1 = resolveIframe(sourceSite + '/' + page +'.php')
+    pagecontent = bitly.getPage(sourceSite + '/' + page1, sourceSite, ua)
+    b64coded = bitly.getBaseEncodedString(pagecontent)
     streamUrl = bitly.getStreamUrl(b64coded)
-    print streamUrl
     return streamUrl
     
 def resolveIframe(page) :
     try :
         if(page[:4] != 'http') :
             page = sourceSite + '/' + page
-        pagecontent = bitly.getPage(page, sourceSite, bitly.getUserAgent())
-        match=re.compile('src="(.+?).php"' ).findall(page)[0]
-        match = (match + '.php')
-        iframesrc = match.search(pagecontent).group(1)
+        userAgent = bitly.getUserAgent()
+        pagecontent = bitly.getPage(page, sourceSite, userAgent)
+        regIframe = re.compile('<iframe(.*?)src="(.*?)"(.*?)><\/iframe>', re.DOTALL)
+        iframesrc = regIframe.search(pagecontent).group(2)
         return iframesrc
     except :
         return page
