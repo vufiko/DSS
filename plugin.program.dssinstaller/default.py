84539d7657ea1546ee57ebe8b9856f8d36fb95c6
dutchsportstreams <patrick@paradiseradio.org>
2016-01-21 19:31:37 +0100

    dss build 1.0.0

---
+++ plugin.program.dssinstaller/default.py
@@ -0,0 +1,322 @@
+import xbmc, xbmcaddon, xbmcgui, xbmcplugin,os,base64,sys,xbmcvfs
+import shutil
+import urllib2,urllib
+import re
+import extract
+import downloader
+import time
+import plugintools
+from addon.common.addon import Addon
+from addon.common.net import Net
+
+
+USER_AGENT = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3'
+addon_id = 'plugin.program.dssinstaller'
+ADDON = xbmcaddon.Addon(id=addon_id)
+AddonID = 'plugin.program.dssinstaller'
+AddonTitle = "DSS Installer"
+dialog = xbmcgui.Dialog()
+net = Net()
+U = ADDON.getSetting('User')
+FANART = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id , 'fanart.jpg'))
+ICON = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id, 'icon.png'))
+ART = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id + '/resources/art/'))
+VERSION = "2016.01.18.01"
+DBPATH = xbmc.translatePath('special://database')
+TNPATH = xbmc.translatePath('special://thumbnails');
+PATH = "DSS Installer"            
+BASEURL = "http://www.dutchsportstreams.com"
+H = 'http://'
+EXCLUDES = ['plugin.program.dssinstaller','script.module.addon.common','repository.build']
+
+def INDEX():
+    addDir('DSS Installatie',BASEURL,2,ART+'icon.png',FANART,'')
+    setView('movies', 'MAIN')
+
+def BUILDMENU():
+    addDir('DSS Installeren','https://www.dropbox.com/s/2yih2wjb6wgtvc0/dssbuild.zip?dl=1',5,ART+'icon.png',FANART,'')
+    setView('movies', 'MAIN')
+
+def MAINTENANCE():
+    addDir('DSS Updaten','-',5,ART+'icon.png',FANART,'')
+    setView('movies', 'MAIN')
+	
+
+#################################
+####### POPUP TEXT BOXES ########
+#################################
+
+def TextBoxes(heading,announce):
+  class TextBox():
+    WINDOW=10147
+    CONTROL_LABEL=1
+    CONTROL_TEXTBOX=5
+    def __init__(self,*args,**kwargs):
+      xbmc.executebuiltin("ActivateWindow(%d)" % (self.WINDOW, )) # activate the text viewer window
+      self.win=xbmcgui.Window(self.WINDOW) # get window
+      xbmc.sleep(500) # give window time to initialize
+      self.setControls()
+    def setControls(self):
+      self.win.getControl(self.CONTROL_LABEL).setLabel(heading) # set heading
+      try: f=open(announce); text=f.read()
+      except: text=announce
+      self.win.getControl(self.CONTROL_TEXTBOX).setText(str(text))
+      return
+  TextBox()
+
+def facebook():
+    TextBoxes('DSS Installer', '[COLOR=red]DSS [/COLOR]')
+        
+
+def WIZARD(name,url,description):
+    path = xbmc.translatePath(os.path.join('special://home/addons','packages'))
+    dp = xbmcgui.DialogProgress()
+    dp.create("DSS Installer","Downloaden ",'', 'Even geduld A.U.B.')
+    lib=os.path.join(path, name+'.zip')
+    try:
+       os.remove(lib)
+    except:
+       pass
+    downloader.download(url, lib, dp)
+    addonfolder = xbmc.translatePath(os.path.join('special://','home'))
+    time.sleep(2)
+    dp.update(0,"", "ZIP wordt uitgepakt - even geduld A.U.B.")
+    print '======================================='
+    print addonfolder
+    print '======================================='
+    extract.all(lib,addonfolder,dp)
+    dialog = xbmcgui.Dialog()
+    dialog.ok("DSS Installer", "Om alle instellingen goed te installeren moet Kodi worden gesloten (force close). Klik op OK om Kodi te sluiten.")
+    killxbmc()
+
+ 
+        
+def OPEN_URL(url):
+    req = urllib2.Request(url)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+    response = urllib2.urlopen(req)
+    link=response.read()
+    response.close()
+    return link
+
+###############################################################
+###FORCE CLOSE KODI - ANDROID ONLY WORKS IF ROOTED#############
+#######LEE @ COMMUNITY BUILDS##################################
+
+def killxbmc():
+    choice = xbmcgui.Dialog().yesno('Sluit (Force Close) Kodi', 'Kodi wordt nu gesloten', 'Wil je doorgaan?', nolabel='Nee, Annuleer',yeslabel='Ja, Sluiten')
+    if choice == 0:
+        return
+    elif choice == 1:
+        pass
+    myplatform = platform()
+    print "Platform: " + str(myplatform)
+    if myplatform == 'osx': # OSX
+        print "############   try osx force close  #################"
+        try: os.system('killall -9 XBMC')
+        except: pass
+        try: os.system('killall -9 Kodi')
+        except: pass
+        dialog.ok("[COLOR=red][B]LET OP!!![/COLOR][/B]", "Als je deze melding ziet is het niet gelukt om Kodi te sluiten.", "Sluit Kodi handmatig door de voeding van de box te halen.",'')
+    elif myplatform == 'linux': #Linux
+        print "############   try linux force close  #################"
+        try: os.system('killall XBMC')
+        except: pass
+        try: os.system('killall Kodi')
+        except: pass
+        try: os.system('killall -9 xbmc.bin')
+        except: pass
+        try: os.system('killall -9 kodi.bin')
+        except: pass
+        dialog.ok("[COLOR=red][B]LET OP!!![/COLOR][/B]", "Als je deze melding ziet is het niet gelukt om Kodi te sluiten.", "Sluit Kodi handmatig door de voeding van de box te halen.",'')
+    elif myplatform == 'android': # Android  
+        print "############   try android force close  #################"
+        try: os.system('adb shell am force-stop org.xbmc.kodi')
+        except: pass
+        try: os.system('adb shell am force-stop org.kodi')
+        except: pass
+        try: os.system('adb shell am force-stop org.xbmc.xbmc')
+        except: pass
+        try: os.system('adb shell am force-stop org.xbmc')
+        except: pass        
+        dialog.ok("[COLOR=yellow][B]INSTALLATIE DSS AFRONDEN[/COLOR][/B]", "Klik de HOME toets op je afstandsbediening en [COLOR=red][B]SLUIT[/COLOR][/B] KODI via Android Instellingen en herstart KODI")
+    elif myplatform == 'windows': # Windows
+        print "############   try windows force close  #################"
+        try:
+            os.system('@ECHO off')
+            os.system('tskill XBMC.exe')
+        except: pass
+        try:
+            os.system('@ECHO off')
+            os.system('tskill Kodi.exe')
+        except: pass
+        try:
+            os.system('@ECHO off')
+            os.system('TASKKILL /im Kodi.exe /f')
+        except: pass
+        try:
+            os.system('@ECHO off')
+            os.system('TASKKILL /im XBMC.exe /f')
+        except: pass
+        dialog.ok("[COLOR=red][B]LET OP!!![/COLOR][/B]", "Als je deze melding ziet is het niet gelukt om Kodi te sluiten.", "Sluit Kodi via de task manager en NIET via ALT F4",'')
+    else: #ATV
+        print "############   try atv force close  #################"
+        try: os.system('killall AppleTV')
+        except: pass
+        print "############   try raspbmc force close  #################" #OSMC / Raspbmc
+        try: os.system('sudo initctl stop kodi')
+        except: pass
+        try: os.system('sudo initctl stop xbmc')
+        except: pass
+        dialog.ok("[COLOR=red][B]WARNING  !!![/COLOR][/B]", "If you\'re seeing this message it means the force close", "was unsuccessful. Please force close XBMC/Kodi [COLOR=lime]DO NOT[/COLOR] exit via the menu.","iOS detected.  Press and hold both the Sleep/Wake and Home button for at least 10 seconds, until you see the Apple logo.")    
+
+        
+def platform():
+    if xbmc.getCondVisibility('system.platform.android'):
+        return 'android'
+    elif xbmc.getCondVisibility('system.platform.linux'):
+        return 'linux'
+    elif xbmc.getCondVisibility('system.platform.windows'):
+        return 'windows'
+    elif xbmc.getCondVisibility('system.platform.osx'):
+        return 'osx'
+    elif xbmc.getCondVisibility('system.platform.atv2'):
+        return 'atv2'
+    elif xbmc.getCondVisibility('system.platform.ios'):
+        return 'ios'
+    
+
+def FRESHSTART(params):
+    plugintools.log("freshstart.main_list "+repr(params)); yes_pressed=plugintools.message_yes_no(AddonTitle,"Wil je de standaard instellingen terug zetten van je Kodi installatie?")
+    if yes_pressed:
+        addonPath=xbmcaddon.Addon(id=AddonID).getAddonInfo('path'); addonPath=xbmc.translatePath(addonPath); 
+        xbmcPath=os.path.join(addonPath,"..",".."); xbmcPath=os.path.abspath(xbmcPath); plugintools.log("freshstart.main_list xbmcPath="+xbmcPath); failed=False
+        try:
+            for root, dirs, files in os.walk(xbmcPath,topdown=True):
+                dirs[:] = [d for d in dirs if d not in EXCLUDES]
+                for name in files:
+                    try: os.remove(os.path.join(root,name))
+                    except:
+                        if name not in ["Addons15.db","MyVideos75.db","Textures13.db","xbmc.log"]: failed=True
+                        plugintools.log("Error removing "+root+" "+name)
+                for name in dirs:
+                    try: os.rmdir(os.path.join(root,name))
+                    except:
+                        if name not in ["Database","userdata"]: failed=True
+                        plugintools.log("Error removing "+root+" "+name)
+            if not failed: plugintools.log("freshstart.main_list All user files removed, you now have a clean install"); plugintools.message(AddonTitle,"Schone Installatie is klaar, je hebt nu een verse Kodi configuratie met KAOSbox Install. Herstart nu je box!")
+            else: plugintools.log("freshstart.main_list User files partially removed"); plugintools.message(AddonTitle,"Schone Installatie is klaar, je hebt nu een verse Kodi configuratie met DSS Install. Herstart nu je box!")
+        except: plugintools.message(AddonTitle,"Problem found","Your settings has not been changed"); import traceback; plugintools.log(traceback.format_exc()); plugintools.log("freshstart.main_list NOT removed")
+        plugintools.add_item(action="",title="Kodi Sluiten",folder=False)
+    else: plugintools.message(AddonTitle,"Er is geen schone installatie gedaan."); plugintools.add_item(action="",title="Klaar",folder=False)
+
+          
+        
+def get_params():
+        param=[]
+        paramstring=sys.argv[2]
+        if len(paramstring)>=2:
+                params=sys.argv[2]
+                cleanedparams=params.replace('?','')
+                if (params[len(params)-1]=='/'):
+                        params=params[0:len(params)-2]
+                pairsofparams=cleanedparams.split('&')
+                param={}
+                for i in range(len(pairsofparams)):
+                        splitparams={}
+                        splitparams=pairsofparams[i].split('=')
+                        if (len(splitparams))==2:
+                                param[splitparams[0]]=splitparams[1]
+                                
+        return param
+
+N = base64.decodestring('')
+T = base64.decodestring('L2FkZG9ucy50eHQ=')
+B = base64.decodestring('')
+F = base64.decodestring('')
+def addDir(name,url,mode,iconimage,fanart,description):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&iconimage="+urllib.quote_plus(iconimage)+"&fanart="+urllib.quote_plus(fanart)+"&description="+urllib.quote_plus(description)
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name, "Plot": description } )
+        liz.setProperty( "Fanart_Image", fanart )
+        if mode==5 :
+            ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
+        else:
+            ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+        return ok
+
+        
+                      
+params=get_params()
+url=None
+name=None
+mode=None
+iconimage=None
+fanart=None
+description=None
+
+
+try:
+        url=urllib.unquote_plus(params["url"])
+except:
+        pass
+try:
+        name=urllib.unquote_plus(params["name"])
+except:
+        pass
+try:
+        iconimage=urllib.unquote_plus(params["iconimage"])
+except:
+        pass
+try:        
+        mode=int(params["mode"])
+except:
+        pass
+try:        
+        fanart=urllib.unquote_plus(params["fanart"])
+except:
+        pass
+try:        
+        description=urllib.unquote_plus(params["description"])
+except:
+        pass
+        
+        
+print str(PATH)+': '+str(VERSION)
+print "Mode: "+str(mode)
+print "URL: "+str(url)
+print "Name: "+str(name)
+print "IconImage: "+str(iconimage)
+
+
+def setView(content, viewType):
+    # set content type so library shows more views and info
+    if content:
+        xbmcplugin.setContent(int(sys.argv[1]), content)
+    if ADDON.getSetting('auto-view')=='true':
+        xbmc.executebuiltin("Container.SetViewMode(%s)" % ADDON.getSetting(viewType) )
+        
+
+if mode==None or url==None or len(url)<1:
+        INDEX()
+
+elif mode==2:
+        BUILDMENU()
+
+elif mode==3:
+        MAINTENANCE()
+		
+	
+elif mode==5:
+        WIZARD(name,url,description)
+
+elif mode==6:        
+	FRESHSTART(params)
+	
+elif mode==8:
+       facebook()
+       
+
+        
+xbmcplugin.endOfDirectory(int(sys.argv[1]))
